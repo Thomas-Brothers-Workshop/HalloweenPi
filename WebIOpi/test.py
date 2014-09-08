7f0e7dd743541730526920d57eb976e154e29b54
perry <perry.thomas@orasi.com>
2014-09-07 22:30:54 -0400

    test env

---
+++ WebIOpi/test.py
@@ -0,0 +1,35 @@
+import webiopi
+import time
+import threading
+import pygame
+import glob
+import random
+
+#Test
+
+webiopi.setDebug()
+
+
+# Called by WebIOPi at script loading
+def setup():
+  # Do nothing
+  pass
+
+# Looped by WebIOPi
+def loop():
+  # Do nothing
+  pass
+
+# Called by WebIOPi at server shutdown
+def destroy():
+  # Do nothing
+  pass
+
+# *********************************************************************************
+# Macros
+# *********************************************************************************
+
+@webiopi.macro
+def ChangeTest(TestString):
+  return TestString + "new"
+
